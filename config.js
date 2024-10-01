983fb27058d716cd3aa465fc93056bd2f010b069
ArilensOfficial <ismaalozturk@gmail.com>
2024-10-01 18:53:50 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  token: '', // Discord bot token'ı buraya
+  guildId: '', // Sunucu ID
+  roles: {
+    free: '', // Free rolü ID'si
+    premium: '', // Premium rolü ID'si
+  },
+  links: {
+    addTimer: '', // Add Timer linki
+    premiumFullUnlock: '', // Premium Full Unlock linki
+    autoAddTimer: '', // Auto Add Timer linki
+    removeLicense: '', // Remove License linki
+  }
+};
